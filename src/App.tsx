a8741d02e7b107cd850aaf9eebcfa79b956d0e54
michal hanau <mh3182100@gmail.com>
2025-06-05 22:32:18 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        <h1>Global Weather</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
